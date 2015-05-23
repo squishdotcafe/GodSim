f4fe2e6fcea9540c1f00c8666ca8fde8111232a6
williambl <william@williambl.com>
2015-05-24 00:39:55 +0100

    Add World Simulator.py
    Adding the Game!

---
+++ World Simulator.py
@@ -0,0 +1,225 @@
+#Game of Life
+#In game statistics based on real life
+#V0.1
+#Author Luke Dunn
+import time
+import turtle
+
+tag = 0
+runs = 0
+print("[Luke Dunn's Game of Life]")
+time.sleep(0.1)
+print()
+#Temperature
+print("[Select an average temperature of the region. (Celcius)]")
+tmp = int(input())
+ox = 0
+co = 0
+ni = 0
+ar = 0
+ot = 0
+tmpd = 1
+tmpb = 1
+print()
+#Population
+print("[Select an estimated population of the region. (1-1000)]")
+ppl = int(input())
+ppla = ppl
+ppld = 0
+pplb = 0
+ppli = 0
+print()
+#Development
+print("[Select a stage of development for the region. (1-5)]")
+dlp = int(input())
+print()
+#
+#
+#
+print()
+active = 1
+while active == 1:
+    cycle = 1
+    while cycle < runs+1:
+        print(cycle)
+        cycle = cycle+1
+        #Population
+        if ppl < 2.5:
+            ppla = 0
+            ppld = ppl
+        if ppla > 1:
+            ppld = (ppld+(ppla*1.00009))*tmpd
+            pplb = pplb+(ppla*1.00019)*tmpb
+            ppla = ppl+int(pplb)-int(ppld)
+        if ppla < 0:
+            ppla = 0
+        if pplb < 0:
+            pplb = 0
+        if ppla == 0:
+            ppld = ppl+pplb
+        #Atmosphere
+        tmp = tmp*1.00000000000924426
+        tmpd = tmp*1.000476190476
+        tmpb = tmp*0.000476190476
+        if tmpd < 0:
+            tmpd = 2-tmp
+        if tmpb < 0:
+            tmpb = 2-tmp
+        time.sleep(1/runs)
+    time.sleep(1)
+    print("[Type /help <page number> for commands to display]")
+    command = 1
+    while command == 1:
+        cmd,tag = input().split()
+        if cmd == '/help':
+            print("##Generic")
+            print("  /run <number of cycles>")
+            print("  /help")
+            print()
+            print("##Stats")
+            print("  /stats population")
+            print("  /stats atmosphere")
+            print("  /stats development")
+            print()
+            print("##Graph")
+            print("  /graph population")
+            print("  /graph atmosphere")
+            print("  /graph development")
+        ##Generic
+        if cmd == '/run':
+            runs = int(tag)
+            command = 0
+        ##Stats
+        if cmd == '/stats':
+            if tag == 'population':
+                print('Deaths:',int(ppld))
+                print('Alive: ',int(ppla))
+                print('Born:  ',int(pplb))
+                print()
+            if tag == 'atmosphere':
+                print('Temperature:',tmp,'*C')
+                print()
+                print('Carbon Dioxide %:',co)
+                print('Oxygen %        :',ox)
+                print('Nitrogen %      :',ni)
+                print('Argon %         :',ar)
+                print('Other %         :',ot)
+                print()
+        ##Graph
+        if cmd == '/graph':
+            if tag == 'population':
+                turtle.speed(0)
+                turtle.hideturtle()
+                turtle.color('white')
+                turtle.left(90)
+                turtle.forward(500)
+                turtle.right(180)
+                turtle.color('black')
+                turtle.right(90)
+                turtle.forward(int(pplb))
+                turtle.left(180)
+                turtle.right(90)
+                turtle.forward(10)
+                turtle.left(90)
+                turtle.color('red')
+                turtle.forward(int(ppld))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppld))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppld))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppld))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppld))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppld))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppld))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppld))
+                turtle.left(90)
+                turtle.color('black')
+                turtle.forward(10)
+                turtle.left(90)
+                turtle.color('blue')
+                turtle.forward(int(ppla))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppla))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppla))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppla))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppla))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppla))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(ppla))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(ppla))
+                turtle.left(90)
+                turtle.color('black')
+                turtle.forward(10)
+                turtle.left(90)
+                turtle.color('green')
+                turtle.forward(int(pplb))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(pplb))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(pplb))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(pplb))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(pplb))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(pplb))
+                turtle.left(90)
+                turtle.forward(1)
+                turtle.left(90)
+                turtle.forward(int(pplb))
+                turtle.right(90)
+                turtle.forward(1)
+                turtle.right(90)
+                turtle.forward(int(pplb))
+                turtle.left(90)
+                turtle.color('black')
+                turtle.forward(10)
+                turtle.exitonclick()
